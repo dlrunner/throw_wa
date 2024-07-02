05681c382173ee03cf8c4a42e51061fc133044fe
syong99 <syong9903222@gmail.com>
2024-07-02 14:43:47 +0900

    임베딩 모델 적용

--- backend/pdf_link_api.py
+++ backend/pdf_link_api.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI, HTTPException, APIRouter
 from pydantic import BaseModel
 import PyPDF2
 from database import Database
+from transformers import BertTokenizer, BertModel , AutoTokenizer, AutoModel
+import torch
 
 router = APIRouter()
 
@@ -19,13 +21,14 @@ db.connect()
 db.create_table()
 
 class PDFUrl(BaseModel):
-    url: str  # pdf_path에서 url로 변경
+    url: str
 
+# 로컬 pdf파일 링크로 텍스트 추출
 def extract_text_from_local_pdf(pdf_url: str) -> str:
     # URL 디코딩
     decoded_path = urllib.parse.unquote(pdf_url)
     
-    # 파일 프로토콜 제거
+    # 파일 프로토콜 제거 이유: file:/// 은 못읽음. 제거 필요
     if decoded_path.startswith("file:///"):
         decoded_path = decoded_path[8:]
     
@@ -43,12 +46,26 @@ def extract_text_from_local_pdf(pdf_url: str) -> str:
     
     return text
 
+# 임베딩 함수
+def embed_text(text: str) -> list :
+    tokenizer = AutoTokenizer.from_pretrained('klue/bert-base') # 모델은 transformers의 klue/bert-base 한국어 임베딩 지원 모델
+    model = AutoModel.from_pretrained('klue/bert-base')         # pip install transformers torch 설치 필요
+
+    inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True, max_length=512)
+    outputs = model(**inputs)
+
+    embeddings = torch.mean(outputs.last_hidden_state, dim=1)
+    return embeddings.detach().numpy().tolist()
+
+
+
 @router.post("/pdf_text")
 async def extract_local_pdf(pdf_url: PDFUrl):
     try:
         extracted_text = extract_text_from_local_pdf(pdf_url.url)
         pdf_id = db.insert_pdf(pdf_url.url,extracted_text)
-        return {"success": True, "text": extracted_text}  # success 필드 추가
+        embedding = embed_text(extracted_text)
+        return {"success": True, "text": extracted_text, "embedding": embedding}  # success 필드 추가 text embedding 값 롹인
     except FileNotFoundError as e:
         raise HTTPException(status_code=404, detail=str(e))
     except Exception as e:
