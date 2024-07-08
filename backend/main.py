6139b7bdae4c242df58e31c573c2fdf1778ebddd
syong99 <syong9903222@gmail.com>
2024-07-08 14:35:40 +0900

    텍스트 임베딩 모델 변경

--- backend/main.py
+++ backend/main.py
@@ -7,11 +7,14 @@ from api.youtube_link_api import router as youtube_router
 from api.pdf_link_api import router as pdf_router
 from api.crawler_link_api import router as crawler_router
 from api.search_api import router as search_router
-from api.image_link_api import router as image_router
+# from api.image_link_api import router as image_router
 
 app = FastAPI()
 
-origins = ['*']
+origins = [
+    "http://localhost:8080",
+    "chrome-extension://fofliiokodhahgckpkfkfgacpkkfjjoc"
+]
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,7 +28,7 @@ app.include_router(youtube_router, prefix="/api")
 app.include_router(pdf_router, prefix="/api")
 app.include_router(crawler_router, prefix="/api")
 app.include_router(search_router, prefix="/api")
-app.include_router(image_router, prefix="/api")  # 새로 추가
+# app.include_router(image_router, prefix="/api")  # 새로 추가
 
 # 앱 실행 코드
 if __name__ == "__main__":
