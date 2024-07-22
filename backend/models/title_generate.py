3a5a939fd834665249ff95197de52391a422937b
pennyw1se200 <qkrrhkdgml200@gmail.com>
2024-07-22 14:39:51 +0900

    gpt-4o-mini 모델 변경완료

--- backend/models/title_generate.py
+++ backend/models/title_generate.py
@@ -12,7 +12,7 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 async def generate_title(text):
     openai.api_key = openai_api_key  # 환경 변수에서 API 키를 가져옴
     response = await openai.ChatCompletion.acreate(
-        model="gpt-3.5-turbo",  # ChatGPT 모델 사용
+        model="gpt-4o-mini",  # ChatGPT 모델 사용
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": f"다음 요약내용을 가지고 제목을 지어주세요. 제목을 생성할 때 '제목:'이라는 단어를 포함하지 마세요. 제목은 한국어로 작성하세요: {text}"}
